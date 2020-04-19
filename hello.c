f736e6135b86639cc3819c20b42fac2a07b79b7f
Jeon yeeun <wjs4199@naver.com>
2020-04-19 21:26:47 +0900

    업데이트 반가움 표시

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@ int main(){
 
 printf("안녕~!\n");
 printf("I'm yeeun! Nice to meet you!\n");
-printf("난 예은이야!\n");
+printf("난 예은이야!만나서 반가워!\n");
 printf("oss 파이팅!\n");
 printf("약간의 수정!\n);
 return 0; 
